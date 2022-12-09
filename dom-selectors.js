4ae0c831eef7887f7711f5fcdf53db4c453cf426
vanshuhassija <33016642+vanshuhassija@users.noreply.github.com>
2022-12-09 22:25:23 +0530

    Create dom-selectors.js

---
+++ dom-selectors.js
@@ -0,0 +1,21 @@
+function changeBackgroundColor(){
+    const paragraphs=document.querySelectorAll(".blue");
+    //Query Selector Only returns the first element That matches the criteria
+    
+    console.log("Paragraphs are",paragraphs)
+}
+
+//document.getElementById("Id That you want to access")
+//document.getElementsByClassName("Class You Want to access")
+//document.getElementsByTagName("Tag That You want to access")
+//document.querySelector("Use Css Like Sytax")---> Will return only one element
+//document.querySelectorAll("Use Css like Syntax") ---> Will return all elements matching 
+//the criteria
+
+
+
+
+
+
+
+changeBackgroundColor();
